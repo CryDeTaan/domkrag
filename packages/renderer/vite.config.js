4a74893e034ebd78e63adb3dc87eb9df7bb0ff9c
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-10-04 18:53:57 +0100

    Enable the vite plugin for electron

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import electron from "vite-plugin-electron";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        entry: "packages/main/main.js",
+        vite: {
+          build: {
+            outDir: "dist/electron",
+          },
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
