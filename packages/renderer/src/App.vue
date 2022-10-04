f476e81eabb104ef75745656908b70dfc8f45e26
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-10-04 19:16:38 +0100

    Update `App.vue` with starting point

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,53 +1,17 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="flex h-screen flex-col">
+    <div class="flex flex-col items-center justify-center border-b-2 py-4">
+      <h1 class="text-5xl font-bold text-gray-700">Electron</h1>
+      <h3 class="mt-2 text-3xl text-gray-500">
+        Inter-Process Communication (IPC) Patterns
+      </h3>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="flex w-full flex-grow items-center justify-center">
+      <!-- Components -->
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script setup>
+// Imports
+</script>
